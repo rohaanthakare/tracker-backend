de65230ce5aba02a5ff151efe5b1ad2e8890f95a
Rohan Thakare <rohaanthakare@gmail.com>
2019-11-21 13:30:41 +0530

    MongoDB Connection remove

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/tracker', {
-    useNewUrlParser: true
-}).then(() => {
-    console.log('Connected to Tracker Database');
-}, error => {
-    console.log('Error while connection to Tracker Database');
-});
 
 const port = 4000;
 const server = app.listen(port, () => {
