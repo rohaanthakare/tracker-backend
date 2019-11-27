98524fb4f80adf19c1255591e582806be9ea2478
Rohan Thakare <rohaanthakare@gmail.com>
2019-11-27 23:28:52 +0530

    Database added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 // Application Imports
+const config = require('./configs/global.config');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,16 @@ const app = express();
 const port = 4000;
 const server = app.listen(port, () => {
     console.log('Connected to Port - ' + port);
+});
+
+// Connect to Database
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Database - ' + config.database);
+});
+
+mongoose.connection.on('error', () => {
+    console.log('Error while connecting to Database - ' + config.database);
 });
 
 app.use('/api', routes);
