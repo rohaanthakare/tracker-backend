9e4afe9d34b647203f1e3017a060f49639fe0df1
Rohan Thakare <rohaanthakare@gmail.com>
2019-12-14 14:30:14 +0530

    Trackerdb connection changes

--- configs/global.config.js
+++ configs/global.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/tracker',
+    database: 'mongodb://heroku_trackerdb:Thakare@1912@ds159926.mlab.com:59926/heroku_h35bb87fmongodb://localhost:27017/tracker',
     token_secret: 'tracker'
 }
