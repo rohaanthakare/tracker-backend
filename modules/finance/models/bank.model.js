180c4b4247e597da54906ab96ce166f5bb6618df
Rohan Thakare <rohaanthakare@gmail.com>
2020-01-03 12:28:35 +0530

    Finance module initial commit

---
+++ modules/finance/models/bank.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const BankSchema = new mongoose.Schema({
+    bankCode: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    bankName: String
+}, {
+    versionKey: false,
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+module.exports = mongoose.model('Bank', BankSchema);
