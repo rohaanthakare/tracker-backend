640736d95cb3e8165df5acf584b1a75f67a491b7
Rohan Thakare <rohaanthakare@gmail.com>
2020-01-06 23:06:59 +0530

    Database pointed to PROD

--- configs/global.config.js
+++ configs/global.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/tracker',
-    token_secret: 'tracker'
-    // database: 'mongodb://trackeradmin:tracker1912@ds159926.mlab.com:59926/heroku_h35bb87f',
+    // database: 'mongodb://localhost:27017/tracker',
+    token_secret: 'tracker',
+    database: 'mongodb://trackeradmin:tracker1912@ds159926.mlab.com:59926/heroku_h35bb87f'
 }
